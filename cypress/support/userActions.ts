27778fceb3057a49e1755f8b50e0c9ef7013105a
Shah rahman <toshah.rahman@gmail.com>
2022-03-05 12:48:59 +0200

    useractions custom command feedback step 1

--- cypress/support/userActions.ts
+++ cypress/support/userActions.ts
@@ -5,6 +5,15 @@ declare namespace Cypress{
          * @param password - takes user passowrd
          */
         login(username: String, password: String): Chainable<Element>
+
+        /**
+         * 
+         * @param name - takes user name
+         * @param email - takes user email
+         * @param subject - takes the subject of the feedback
+         * @param description - takes the description of the feedback
+         */
+        feedback( name:String, email:String, subject:String, description:String)
     }
 }
 
@@ -12,4 +21,8 @@ Cypress.Commands.add('login',(username, password)=>{
     cy.get('#user_login').type('username')
     cy.get('#user_password').type('password')
     cy.contains('Sign in').click()
+})
+
+Cypress.Commands.add('feedback',()=>{
+    
 })
