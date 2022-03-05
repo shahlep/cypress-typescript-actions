934820d949469e30365ac8f3a005e2b58650c2b4
Shah rahman <toshah.rahman@gmail.com>
2022-03-05 12:11:37 +0200

    useractions custom command

---
+++ cypress/support/userActions.ts
@@ -0,0 +1,9 @@
+declare namespace Cypress{
+    interface Chainable{
+        /**
+         * @param username - takes user name or id
+         * @param password - takes user passowrd
+         */
+        login(username: String, password: String): Chainable<Element>
+    }
+}
